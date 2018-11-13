f2b1632a4f1ef314f7006cfad863fdc2e68b1297
Fredy Roman Vazquez Geronimo <crazzy-rock@live.com.mx>
2018-11-13 11:34:36 -0600

    Referencia de clases se agrega las referencias de las clases en el aplication

--- src/main/java/com/integration/contact/ContactApplication.java
+++ src/main/java/com/integration/contact/ContactApplication.java
@@ -2,7 +2,15 @@ package com.integration.contact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
+import com.integration.repository.ContactRepository;
+
+@EntityScan("com.integration.model")
+@ComponentScan(basePackages = "com.integration")
+@EnableReactiveMongoRepositories(basePackageClasses = ContactRepository.class)
 @SpringBootApplication
 public class ContactApplication {
 
