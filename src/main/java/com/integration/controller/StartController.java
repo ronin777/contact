89ec8f2d0892875248dc7da7bac4fa6ef3dc4ca3
Fredy Roman Vazquez Geronimo <crazzy-rock@live.com.mx>
2018-11-13 14:08:13 -0600

    Se crean servicios servicios grub y se modifica version de libreria

--- src/main/java/com/integration/controller/StartController.java
+++ src/main/java/com/integration/controller/StartController.java
@@ -1,20 +1,12 @@
 package com.integration.controller;
 
 import javax.validation.Valid;
-
+import com.integration.model.Contact;
+import com.integration.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.integration.model.Contact;
-import com.integration.repository.ContactRepository;
-
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -30,7 +22,7 @@ public class StartController {
 	@Autowired
 	private ContactRepository contactRepository;
 
-	@GetMapping("/contact")
+	@GetMapping("/contacts")
 	public Flux<Contact> getAllContacts() {
 		return contactRepository.findAll();
 	}
@@ -42,15 +34,13 @@ public class StartController {
 
 	@GetMapping("/contact/{id}")
 	public Mono<ResponseEntity<Contact>> getContactById(@PathVariable(value = "id") String id) {
-		return contactRepository.findById(id).
-				map(savedContact -> ResponseEntity.ok(savedContact))
+		return contactRepository.findById(id).map(savedContact -> ResponseEntity.ok(savedContact))
 				.defaultIfEmpty(ResponseEntity.notFound().build());
 	}
 
 	@DeleteMapping("/contactdel/{id}")
 	public Mono<ResponseEntity<Void>> deleteContact(@PathVariable(value = "id") String id) {
-		return contactRepository.findById(id).
-				flatMap(existingContact -> contactRepository.delete(existingContact))
+		return contactRepository.findById(id).flatMap(existingContact -> contactRepository.delete(existingContact))
 				.then(Mono.just(new ResponseEntity<Void>(HttpStatus.OK)))
 				.defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
 	}
